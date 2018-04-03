15c9e400978a637520fdc74b8296d73e2f238356
pearisgreen <grproessl@web.de>
2018-04-03 23:25:13 +0200

    update

--- src/scene/scene.c
+++ src/scene/scene.c
@@ -10,17 +10,21 @@
 #undef HANDLEOBJECT_C
 #endif
 
-#define HANDLE_SIZE 6
+#define HANDLE_SIZE 7
 #define H_UPDATE 0
 #define H_DRAW 1
 #define H_START 2
 #define H_STOP 3
 #define H_EVENT 4
 #define H_CLICK 5
+#define H_CHANGE_STATE 6
+
 
 struct scene
 {
     void *super;
+
+    int state;
 
     struct handle *scene_handle;
 };
@@ -33,6 +37,8 @@ void scene_remove_handle(struct scene *this, int handle_type, struct handle_obje
 
 void scene_dispatch_handle(struct scene *this, int handle_type, void *data);
 
+void scene_change_state(struct scene *this, int new_state);
+
 void scene_delete(struct scene *this);
 
 #endif
@@ -44,6 +50,7 @@ void scene_init(struct scene *this, void *super)
 {
     this->super = super;
     this->scene_handle = malloc(sizeof(struct handle));
+    this->state = 0;
     handle_init(this->scene_handle, HANDLE_SIZE);
 }
 
@@ -60,6 +67,12 @@ void scene_remove_handle(struct scene *this, int handle_type, struct handle_obje
 void scene_dispatch_handle(struct scene *this, int handle_type, void *data)
 {
     handle_dispatch(this->scene_handle, handle_type, data);
+}
+
+void scene_change_state(struct scene *this, int new_state)
+{
+    this->state = new_state;
+    handle_dispatch(this->scene_handle, H_CHANGE_STATE, new_state);
 }
 
 void scene_delete(struct scene *this)
